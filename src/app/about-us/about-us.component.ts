110105007cff1b24daad20b8917f42a45830e952
qafar-hunbetov <qafarhunbetov032@gmail.com>
2023-09-23 13:58:13 +0400

    transition-error

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { faWifi } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
-
+  faWifi=faWifi
+  faStar=faStar
+  faBoxOpen=faBoxOpen
 }
