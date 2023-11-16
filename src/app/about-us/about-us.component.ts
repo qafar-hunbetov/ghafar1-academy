754333de2f1c61e9605e3ec9606a4e433c80cccb
qafar-hunbetov <qafarhunbetov032@gmail.com>
2023-11-16 20:22:43 +0400

    commit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -15,10 +15,11 @@ export class AboutUsComponent {
   faBoxOpen=faBoxOpen
 
   images = [
-        { source: '/assets/img/car-12.png', alt: 'Image 1' },
+        { source: '/https://demoxml.com/html/carmax/images/partner-logo3.png', alt: 'Image 1' },
         { source:  '/assets/img/car-13.png', alt: 'Image 2' },
         { source: '/assets/img/car-14.png', alt: 'Image 3' },
         { source: '/assets/img/car-15.png', alt: 'Image 3' },
+    { source: '/assets/img/car-12.png', alt: 'Image 1' }
       ];
         
  
